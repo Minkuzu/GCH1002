a90e242c2a59adeedf0f5018ae87e72316ce798e
Mink <minhndgdh190046@fpt.edu.vn>
2023-01-04 13:19:52 +0700

    Add files via upload

---
+++ databaseHandler.js
@@ -0,0 +1,37 @@
+var mongoClient = require('mongodb').MongoClient
+var url = 'mongodb+srv://Minkuzu:7898797799as@cluster0.cxg3ckn.mongodb.net/test'
+const { ObjectId } = require('bson')
+
+async function insertProduct(newProduct) {
+    let client = await mongoClient.connect(url)
+    let db = client.db("Mink")
+    let id = await db.collection("products").insertOne(newProduct)
+    return id
+}
+
+async function getAllProducts() {
+    let client = await mongoClient.connect(url)
+    let db = client.db("Mink")
+    let results = await db.collection("products").find({}).sort({ name: 1 }).toArray()
+    return results
+}
+
+async function deleteProductById(id) {
+    let client = await mongoClient.connect(url)
+    let db = client.db("Mink")
+    await db.collection("products").deleteOne({ _id: ObjectId(id) })
+}
+
+async function findProductById(id) {
+    let client = await mongoClient.connect(url)
+    let db = client.db("Mink")
+    let prod = await db.collection("products").findOne({ _id: ObjectId(id) })
+    return prod
+}
+
+async function updateProduct(id, newProduct) {
+    let client = await mongoClient.connect(url)
+    let db = client.db("Mink")
+    await db.collection("products").updateOne({ _id: ObjectId(id) }, { $set: newProduct })
+}
+module.exports = { insertProduct, getAllProducts, deleteProductById, findProductById, updateProduct }
---
+++ main.js
@@ -0,0 +1,114 @@
+const { ObjectId } = require('bson')
+var express = require('express')
+const { insertProduct, getAllProducts, deleteProductById, findProductById, updateProduct } = require('./databaseHandler')
+var app = express()
+var mongo = require('mongodb')
+var path = require('path')
+
+var mongoClient = require('mongodb').MongoClient
+var url = 'mongodb+srv://Minkuzu:7898797799as@cluster0.cxg3ckn.mongodb.net/test'
+
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/delete', async(req, res) => {
+    const id = req.query.id
+    await deleteProductById(id)
+    res.redirect('/')
+})
+
+app.post('/sort', async(req, res) => {
+    let name = req.body.txtName
+    let client = await mongoClient.connect(url);
+    let db = client.db("Mink");
+    //For substring search, case insensitive
+    let results = await db.collection("products").
+    find().sort({ name: 1 }).toArray()
+    console.log(results)
+    res.render('home', { results: results })
+})
+
+app.post('/search', async(req, res) => {
+    let name = req.body.txtSearch
+    let client = await mongoClient.connect(url);
+    let db = client.db("Mink");
+    //For substring search, case insensitive
+    let results = await db.collection("products").
+    find({ 'name': new RegExp(name, 'i') }).toArray()
+    console.log(results)
+    res.render('home', { results: results })
+})
+
+// app.get('/all', async(req, res) => {
+//     let results = await getAllProducts()
+//     res.render('allProduct', { results: results })
+// })
+
+app.post('/update', async(req, res) => {
+    let id = req.body.id
+    let objectId = ObjectId(id)
+    let name = req.body.txtName
+    let price = Number(req.body.txtPrice)
+    let desc = req.body.txtDesc
+    let picURL = req.body.txtPic
+    if (isNaN(name) && name.length < 3) {
+        res.render('home')
+    } else if (isNaN(price) || price < 0 || price > 999) {
+        res.render('home')
+    } else {
+        let newProduct = {
+            name: name,
+            price: Number.parseFloat(price),
+            desc: desc,
+            picture: picURL
+        }
+        await updateProduct(id, newProduct)
+        res.redirect('/')
+    }
+})
+
+app.get('/edit', async(req, res) => {
+    const id = req.query.id
+    let objectId = ObjectId(id)
+    let prod = await findProductById(id)
+    console.log(prod)
+    res.render('edit', { prod: prod })
+})
+
+app.post('/new', async(req, res) => {
+    const name = req.body.txtName
+    const price = Number.parseFloat(req.body.txtPrice)
+    const desc = req.body.txtDesc
+    const picURL = req.body.txtPic
+    if (isNaN(name) && name.length < 3) {
+        res.render('newProduct')
+    } else if (isNaN(price) || price < 0 || price > 999) {
+        res.render('newProduct')
+    } else {
+        const newProduct = {
+            name: name,
+            price: Number.parseFloat(price),
+            desc: desc,
+            picture: picURL
+        }
+        let id = await insertProduct(newProduct)
+        console.log(id)
+        res.redirect('/')
+    }
+})
+
+app.get('/new', (req, res) => {
+    res.render('newProduct')
+})
+
+app.get('/', async(req, res) => {
+    let results = await getAllProducts()
+    res.render('home', { results: results })
+})
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log("Server is running!")
